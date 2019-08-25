0e4568e39340165aa70613e0083566c0e30fff14
denningkevin <denningkev@gmail.com>
2019-08-25 23:51:34 +0300

    add toggle function to show hihhen elements on icon click

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $("#show1").click(function () {
+    $("#hide1").toggle();
+    $("#show1").toggle();
+  });
+  $("#show2").click(function () {
+    $("#hide2").toggle();
+    $("#show2").toggle();
+  });
+  $("#show3").click(function () {
+    $("#hide3").toggle();
+    $("#show3").toggle();
+  });
+  
+})
